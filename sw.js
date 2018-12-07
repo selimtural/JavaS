ab149e8b176203c7c3be9d855e4a1bea95341124
Unknown <selim_tural@hotmail.com.tr>
2018-12-07 09:46:05 +0300

    pwa

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE ='JavaS'
+
+function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  console.log('selimtural', req.url);
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
